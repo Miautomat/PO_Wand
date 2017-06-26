0d78e7ecf40002533cf73f9f3728aac108a02039
Sebastian Diedrich <sebastian.diedrich@haw-hamburg.de>
2017-06-26 14:26:09 +0200

    Write on Wall
    In the moment only "Hallo" possible

--- FlexSensorNeoPixel/FlexSensorNeoPixel.ino
+++ FlexSensorNeoPixel/FlexSensorNeoPixel.ino
@@ -40,8 +40,8 @@ int left_upper_corner1[NUM_EACH_LINE] = {59, 58, 57, 56, 55, 54, 53, 52, 51, 50,
 int left_upper_corner2[NUM_EACH_LINE] = {60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89};
 int left_upper_corner3[NUM_EACH_LINE] = {179, 178, 177, 176, 175, 174, 173, 172, 171, 170, 169, 168, 167, 166, 165, 164, 163, 162, 161, 160, 159, 158, 157, 156, 155, 154, 153, 152, 151, 150};
 int left_upper_all[NUM_EACH_CORNER] = {59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30,
-                                      60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
-                                      179, 178, 177, 176, 175, 174, 173, 172, 171, 170, 169, 168, 167, 166, 165, 164, 163, 162, 161, 160, 159, 158, 157, 156, 155, 154, 153, 152, 151, 150
+                                       60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
+                                       179, 178, 177, 176, 175, 174, 173, 172, 171, 170, 169, 168, 167, 166, 165, 164, 163, 162, 161, 160, 159, 158, 157, 156, 155, 154, 153, 152, 151, 150
                                       };
 
 //Left-lower-corner (yellow)
@@ -51,13 +51,13 @@ int left_lower_corner3[NUM_EACH_LINE] = {300, 301, 302, 303, 304, 305, 306, 307,
 int left_lower_all[NUM_EACH_CORNER] = {180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209,
                                        299, 298, 297, 296, 295, 294, 293, 292, 291, 290, 289, 288, 287, 286, 285, 284, 283, 282, 281, 280, 279, 278, 277, 276, 275, 274, 273, 272, 271, 270,
                                        300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328, 329
-                                       };
+                                      };
 
 //Right-upper-corner (black)
-int right_upper_corner1[NUM_EACH_LINE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29};
+int right_upper_corner1[NUM_EACH_LINE] = {29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 int right_upper_corner2[NUM_EACH_LINE] = {90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119};
 int right_upper_corner3[NUM_EACH_LINE] = {149, 148, 147, 146, 145, 144, 143, 142, 141, 140, 139, 138, 137, 136, 135, 134, 133, 132, 131, 130, 129, 128, 127, 126, 125, 124, 123, 122, 121, 120};
-int right_upper_all[NUM_EACH_CORNER] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+int right_upper_all[NUM_EACH_CORNER] = {29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
                                         90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119,
                                         149, 148, 147, 146, 145, 144, 143, 142, 141, 140, 139, 138, 137, 136, 135, 134, 133, 132, 131, 130, 129, 128, 127, 126, 125, 124, 123, 122, 121, 120
                                        };
@@ -83,12 +83,18 @@ int initBlue;
 int initGreen;
 int initBlack;
 
+char text[10];
+
 //Set the mode
 /*
    1 = test mode
    2 = all LEDs in each Corner
    3 = theaterChaseRainbow
    4 = theaterChaseRainbowEachCorner
+   5 = rainbow
+   6 = rainbowCycle
+   7 = fill corner
+   8 = Write Text
 */
 int mode = 2;
 
@@ -116,6 +122,9 @@ void setup() {
 
   Serial.begin(9600);       //Begin serial communication
 
+  //Write on wall method:
+  strcpy(text, "Hallo!");
+
 }
 
 void loop() {
@@ -142,10 +151,32 @@ void loop() {
       break;
     case 4:
       // Code
-      //theaterChaseRainbowEachCorner2(flexPinA2, initYellow, left_lower_all, 0);
-      theaterChaseRainbowEachCorner3(flexPinA3, initBlue, right_lower_all, 0);
-      //theaterChaseRainbowEachCorner4(flexPinA4, initGreen, left_upper_all, 0);
-      //theaterChaseRainbowEachCorner5(flexPinA5, initBlack, right_upper_all, 0);
+      theaterChaseRainbowEachCorner(flexPinA2, initYellow, left_lower_all, 0);
+      theaterChaseRainbowEachCorner(flexPinA3, initBlue, right_lower_all, 0);
+      theaterChaseRainbowEachCorner(flexPinA4, initGreen, left_upper_all, 0);
+      theaterChaseRainbowEachCorner(flexPinA5, initBlack, right_upper_all, 0);
+      strip.show();
+      break;
+    case 5:
+      // Code
+      rainbow(20);
+      break;
+    case 6:
+      // Code
+      rainbowCycle(20);
+      break;
+    case 7:
+      // Code
+      fillCorner(flexPinA2, initYellow, left_lower_all, 50);
+      fillCorner(flexPinA3, initBlue, right_lower_all, 50);
+      fillCorner(flexPinA4, initGreen, left_upper_all, 50);
+      fillCorner(flexPinA5, initBlack, right_upper_all, 50);
+      strip.show();
+      break;
+    case 8:
+      // Code
+      writeOnWall(500);
+      strip.show();
       break;
     default:
       // Code
@@ -163,7 +194,7 @@ void touched(int value, int init, int ledPin) {
   }
 }
 
-//single-LED in each corner
+//MODE 1: single-LED in each corner
 void touchedNEO(int value, int init, int startLED) {
   if (value > (init + THRESHOLD)) {
     strip.setPixelColor(startLED, strip.Color(0, 255, 0));
@@ -172,7 +203,7 @@ void touchedNEO(int value, int init, int startLED) {
   }
 }
 
-//multi-LED in each corner
+//MODE 2: multi-LED in each corner
 void touchedNEOmulti(int value, int init, int topLine[NUM_EACH_LINE], int middleLine[NUM_EACH_LINE], int downLine[NUM_EACH_LINE]) {
   if (value > (init + THRESHOLD)) {
     //for-loop through the int-array
@@ -216,11 +247,6 @@ void touchedNEOmulti(int value, int init, int topLine[NUM_EACH_LINE], int middle
   }
 }
 
-//Hauptprogramm
-void feelTheTouch() {
-
-}
-
 //Test with 4 white LEDs on the breadboard
 void flexLEDTest() {
   valueYellowA2 = analogRead(flexPinA2);        //Read and save analog value from potentiometer
@@ -242,8 +268,165 @@ void flexLEDTest() {
   Serial.println();
 }
 
+//MODE 8: Write Text on Wall
+void writeOnWall(int wait) {
+  for (int i = 0; i < 10; i++) {
+    if (text[i] == 'H') {
+      //Left of the 'H'
+      strip.setPixelColor(59, strip.Color(0, 255, 0));
+      strip.setPixelColor(60, strip.Color(0, 255, 0));
+      strip.setPixelColor(179, strip.Color(0, 255, 0));
+      strip.setPixelColor(180, strip.Color(0, 255, 0));
+      strip.setPixelColor(299, strip.Color(0, 255, 0));
+      strip.setPixelColor(300, strip.Color(0, 255, 0));
+      //Right of the 'H'
+      strip.setPixelColor(30, strip.Color(0, 255, 0));
+      strip.setPixelColor(89, strip.Color(0, 255, 0));
+      strip.setPixelColor(150, strip.Color(0, 255, 0));
+      strip.setPixelColor(209, strip.Color(0, 255, 0));
+      strip.setPixelColor(270, strip.Color(0, 255, 0));
+      strip.setPixelColor(329, strip.Color(0, 255, 0));
+      for (int i = 0; i < NUM_EACH_LINE; i++) {
+        strip.setPixelColor(left_upper_corner3[i], strip.Color(0, 255, 0));
+      }
+      strip.show();
+      delay(wait);
+      clareAllLeft();
+      delay(wait);
+    }
+    if (text[i] == 'a') {
+      //Left of the 'a'
+      strip.setPixelColor(59, strip.Color(0, 255, 0));
+      strip.setPixelColor(60, strip.Color(0, 255, 0));
+      strip.setPixelColor(179, strip.Color(0, 255, 0));
+      strip.setPixelColor(180, strip.Color(0, 255, 0));
+      strip.setPixelColor(299, strip.Color(0, 255, 0));
+      strip.setPixelColor(300, strip.Color(0, 255, 0));
+      //Right of the 'a'
+      strip.setPixelColor(30, strip.Color(0, 255, 0));
+      strip.setPixelColor(89, strip.Color(0, 255, 0));
+      strip.setPixelColor(150, strip.Color(0, 255, 0));
+      strip.setPixelColor(209, strip.Color(0, 255, 0));
+      strip.setPixelColor(270, strip.Color(0, 255, 0));
+      strip.setPixelColor(329, strip.Color(0, 255, 0));
+      for (int i = 0; i < NUM_EACH_LINE; i++) {
+        strip.setPixelColor(left_upper_corner3[i], strip.Color(0, 255, 0));
+      }
+      for (int i = 0; i < NUM_EACH_LINE; i++) {
+        strip.setPixelColor(left_upper_corner1[i], strip.Color(0, 255, 0));
+      }
+      strip.show();
+      delay(wait);
+      clareAllLeft();
+      delay(wait);
+    }
+    if (text[i] == 'l') {
+      //Left of the 'l'
+      strip.setPixelColor(59, strip.Color(0, 255, 0));
+      strip.setPixelColor(60, strip.Color(0, 255, 0));
+      strip.setPixelColor(179, strip.Color(0, 255, 0));
+      strip.setPixelColor(180, strip.Color(0, 255, 0));
+      strip.setPixelColor(299, strip.Color(0, 255, 0));
+      strip.setPixelColor(300, strip.Color(0, 255, 0));
+      for (int i = 0; i < NUM_EACH_LINE; i++) {
+        strip.setPixelColor(left_lower_corner3[i], strip.Color(0, 255, 0));
+      }
+      strip.show();
+      delay(wait);
+      clareAllLeft();
+      delay(wait);
+    }
+    if (text[i] == 'o') {
+      //Left of the 'o'
+      strip.setPixelColor(59, strip.Color(0, 255, 0));
+      strip.setPixelColor(60, strip.Color(0, 255, 0));
+      strip.setPixelColor(179, strip.Color(0, 255, 0));
+      strip.setPixelColor(180, strip.Color(0, 255, 0));
+      strip.setPixelColor(299, strip.Color(0, 255, 0));
+      strip.setPixelColor(300, strip.Color(0, 255, 0));
+      //Right of the 'o'
+      strip.setPixelColor(30, strip.Color(0, 255, 0));
+      strip.setPixelColor(89, strip.Color(0, 255, 0));
+      strip.setPixelColor(150, strip.Color(0, 255, 0));
+      strip.setPixelColor(209, strip.Color(0, 255, 0));
+      strip.setPixelColor(270, strip.Color(0, 255, 0));
+      strip.setPixelColor(329, strip.Color(0, 255, 0));
+      for (int i = 0; i < NUM_EACH_LINE; i++) {
+        strip.setPixelColor(left_upper_corner1[i], strip.Color(0, 255, 0));
+      }
+      for (int i = 0; i < NUM_EACH_LINE; i++) {
+        strip.setPixelColor(left_lower_corner3[i], strip.Color(0, 255, 0));
+      }
+      strip.show();
+      delay(wait);
+      clareAllLeft();
+      delay(wait);
+    }
+    if (text[i] == '!') {
+      for (int i = 0; i < NUM_EACH_CORNER; i++) {
+        strip.setPixelColor(left_upper_all[i], strip.Color(0, 0, 255));
+      }
+      for (int i = 0; i < NUM_EACH_CORNER; i++) {
+        strip.setPixelColor(left_lower_all[i], strip.Color(0, 0, 255));
+      }
+      strip.show();
+      delay(wait);
+      clareAllLeft();
+      delay(wait);
+    }
+  }
+}
+//CLEAR ALL LEFT
+void clareAllLeft() {
+  switchOffCorner(left_upper_all);
+  switchOffCorner(left_lower_all);
+  strip.show();
+}
+
+//MODE 7: Fill Corner
+void fillCorner(int flexPin, int init, int corner[NUM_EACH_CORNER], uint8_t wait) {
+  if (analogRead(flexPin) > (init + THRESHOLD)) {
+    for (int i = 0; i < NUM_EACH_CORNER; i++) {
+      if (analogRead(flexPin) > (init + THRESHOLD)) {
+        strip.setPixelColor(corner[i], strip.Color(0, 255, 0));
+        delay(wait);
+        strip.show();
+      }
+      else {
+        switchOffCorner(corner);
+      }
+    }
+  }
+}
+
+//MODE 6: Raibbow-Cycle
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//MODE 5: Rainbow
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for (j = 0; j < 256; j++) {
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
 //MODE 4: Theatre-style crawling lights with rainbow effect (each corner alone)
-void theaterChaseRainbowEachCorner2(int flexPin, int init, int corner[NUM_EACH_CORNER], uint8_t wait) {
+void theaterChaseRainbowEachCorner(int flexPin, int init, int corner[NUM_EACH_CORNER], uint8_t wait) {
   if (analogRead(flexPin) > (init + THRESHOLD)) {
     for (int j = 0; j < 256; j++) {   // cycle all 256 colors in the wheel
       for (int q = 0; q < 3; q++) {
@@ -253,120 +436,21 @@ void theaterChaseRainbowEachCorner2(int flexPin, int init, int corner[NUM_EACH_C
             strip.setPixelColor(corner[i + q], Wheel( (i + j) % 255)); //turn every third pixel on
           }
           else {
-            switchOffCorner2(corner);
+            switchOffCorner(corner);
             break;
           }
         }
         strip.show();
-
-        //delay(wait);
       }
     }
-  }
-  else {
-    switchOffCorner2(corner);
-  }
-}
-void theaterChaseRainbowEachCorner3(int flexPin, int init, int corner[NUM_EACH_CORNER], uint8_t wait) {
-  if (analogRead(flexPin) > (init + THRESHOLD)) {
-    for (int j = 0; j < 256; j++) {   // cycle all 256 colors in the wheel
-      for (int q = 0; q < 3; q++) {
-        for (uint16_t i = 0; i < NUM_EACH_CORNER - 3; i = i + 3) { //-3 => otherwise you are leaving the corner!!!
-          //Check every time, otherwise the color-wheel will not stop immediately
-          if (analogRead(flexPin) > (init + THRESHOLD)) {
-            strip.setPixelColor(corner[i + q], Wheel( (i + j) % 255)); //turn every third pixel on
-          }
-          else {
-            switchOffCorner3(corner);
-            break;
-          }
-        }
-        strip.show();
-
-        //delay(wait);
-      }
-    }
-  }
-  else {
-    switchOffCorner3(corner);
-  }
-}
-void theaterChaseRainbowEachCorner4(int flexPin, int init, int corner[NUM_EACH_CORNER], uint8_t wait) {
-  if (analogRead(flexPin) > (init + THRESHOLD)) {
-    for (int j = 0; j < 256; j++) {   // cycle all 256 colors in the wheel
-      for (int q = 0; q < 3; q++) {
-        for (uint16_t i = 0; i < NUM_EACH_CORNER - 3; i = i + 3) { //-3 => otherwise you are leaving the corner!!!
-          //Check every time, otherwise the color-wheel will not stop immediately
-          if (analogRead(flexPin) > (init + THRESHOLD)) {
-            strip.setPixelColor(corner[i + q], Wheel( (i + j) % 255)); //turn every third pixel on
-          }
-          else {
-            switchOffCorner4(corner);
-            break;
-          }
-        }
-        strip.show();
-
-        //delay(wait);
-      }
-    }
-  }
-  else {
-    switchOffCorner4(corner);
-  }
-}
-void theaterChaseRainbowEachCorner5(int flexPin, int init, int corner[NUM_EACH_CORNER], uint8_t wait) {
-  if (analogRead(flexPin) > (init + THRESHOLD)) {
-    for (int j = 0; j < 256; j++) {   // cycle all 256 colors in the wheel
-      for (int q = 0; q < 3; q++) {
-        for (uint16_t i = 0; i < NUM_EACH_CORNER - 3; i = i + 3) { //-3 => otherwise you are leaving the corner!!!
-          //Check every time, otherwise the color-wheel will not stop immediately
-          if (analogRead(flexPin) > (init + THRESHOLD)) {
-            strip.setPixelColor(corner[i + q], Wheel( (i + j) % 255)); //turn every third pixel on
-          }
-          else {
-            switchOffCorner5(corner);
-            break;
-          }
-        }
-        strip.show();
-
-        //delay(wait);
-      }
-    }
-  }
-  else {
-    switchOffCorner5(corner);
   }
 }
 //SWITCH OFF all LEDs of the corner
-void switchOffCorner2(int corner[NUM_EACH_CORNER]) {
+void switchOffCorner(int corner[NUM_EACH_CORNER]) {
   //for-loop through the int-array and switch off all LEDs
   for (int i = 0; i < NUM_EACH_CORNER; i++) {
     strip.setPixelColor(corner[i], strip.Color(0, 0, 0));
   }
-  strip.show();
-}
-void switchOffCorner3(int corner[NUM_EACH_CORNER]) {
-  //for-loop through the int-array and switch off all LEDs
-  for (int i = 0; i < NUM_EACH_CORNER; i++) {
-    strip.setPixelColor(corner[i], strip.Color(0, 0, 0));
-  }
-  strip.show();
-}
-void switchOffCorner4(int corner[NUM_EACH_CORNER]) {
-  //for-loop through the int-array and switch off all LEDs
-  for (int i = 0; i < NUM_EACH_CORNER; i++) {
-    strip.setPixelColor(corner[i], strip.Color(0, 0, 0));
-  }
-  strip.show();
-}
-void switchOffCorner5(int corner[NUM_EACH_CORNER]) {
-  //for-loop through the int-array and switch off all LEDs
-  for (int i = 0; i < NUM_EACH_CORNER; i++) {
-    strip.setPixelColor(corner[i], strip.Color(0, 0, 0));
-  }
-  strip.show();
 }
 
 //MODE 3: Theatre-style crawling lights with rainbow effect (whole wall)
